bb28fbc718a684b6aafb93095d7c4de6c8c9d283
Tijana Markovic <tijana.vujicevic@gmail.com>
2023-10-03 18:21:44 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SingUpComponent } from './components/sign-up/sing-up.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'product-detail/:id', component: ProductDetailComponent },
+  { path: 'log-in', component: LogInComponent },
+  { path: 'sign-up', component: SingUpComponent },
+  { path: 'about', component: AboutComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SocialBarComponent } from './components/social-bar/social-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { SingUpComponent } from './components/sign-up/sing-up.component';
+import { UserService } from './services/userService';
+import { AboutComponent } from './components/about/about.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SocialBarComponent,
+    FooterComponent,
+    ProductsComponent,
+    LogInComponent,
+    ProductDetailComponent,
+    SingUpComponent,
+    AboutComponent,
+
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
